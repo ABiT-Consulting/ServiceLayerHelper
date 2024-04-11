44790405040c39e7e823af15eb28b355b33af622
Muhammad Imran <imran@abitcons.com>
2024-04-11 01:50:45 -0700

    save

--- ServiceLayerHelper/obj/Debug/net7.0/ServiceLayerHelper.AssemblyInfo.cs
+++ ServiceLayerHelper/obj/Debug/net7.0/ServiceLayerHelper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceLayerHelper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94cced3673fe7f483054b28ae64ad51dca597d24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e902635be4e331e40edf9ddc5db1ae354ed9c94c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceLayerHelper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceLayerHelper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
